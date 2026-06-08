5aeef26bc3b3b2b73cc71552edfc7e8b49ee0faa
Amantomar7 <aman22061@iiitd.ac.in>
2026-06-08 21:47:46 +0530

    Initial structure of proj

---
+++ src/grad-cam/__init__.py
@@ -0,0 +1,3 @@
+from .gradcam import GradCAM, grad_cam_from_pytorch_model
+
+__all__ = ["GradCAM", "grad_cam_from_pytorch_model"]
---
+++ src/grad-cam/gradcam.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def grad_cam_from_pytorch_model():
+    print("doing grad cam!")
+    pass
+
+def gradCAM():
+    pass
